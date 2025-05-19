63cb4934670b3aeeb482bbdaff79ed16f2ee535b
fuadmuchtar <tech.fuad99@gmail.com>
2025-05-20 02:35:53 +0700

    feat: add Bootstrap and React-Bootstrap integration, update Navbar component, and create Product page

--- rextech-app/src/app/layout.tsx
+++ rextech-app/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from "@/components/Navbar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+        <NavbarComponent />
         {children}
       </body>
     </html>
